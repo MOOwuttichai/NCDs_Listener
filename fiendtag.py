0bb63da6d0ce9ae6660df8f14a0ac6ed118dd406
oofsss <wuttichai.kha@kkumail.com>
2024-06-21 16:55:45 +0700

    update 21/06/2024 ทำtag ระบุอาการกับโรค เเละสร้างปุ่มกรองอาการเเละโรคเบื้องต้นอัตโนมัติ พร้อมทั้งเก็บข้อมูลในไฟล์ CSV

--- fiendtag.py
+++ fiendtag.py
@@ -14,12 +14,31 @@ def index():
 @app.route("/ajax_add",methods=["POST","GET"])
 def ajax_add():
   if request.method == 'POST':
+        # ข้อมูลที่ได้รับจากการกรอง
         skill = request.form['skill']
         symptom  = request.form['symptom'] 
         name_cancer=skill.split(', ')
         name_symptom = symptom.split(', ')
-        print(name_cancer)
-        print(name_symptom)
+        # ข้อมูลอาการเพิ่มเติม
+        data_defu = pd.read_csv('name_cancer_and_symptoms (2).csv')
+        data_defu_sym = data_defu[['Key_symptoms_TH','Values_symptoms_TH']].dropna()
+        data_value_TH = pd.DataFrame()
+        data_symptoms_TH = pd.DataFrame()
+        data_value_TH['name_cancarTH_se']=name_cancer
+        data_symptoms_TH['Key_symptoms_TH'] =name_symptom
+        data_symptoms_TH_1 = data_symptoms_TH.merge(data_defu_sym, how='left',on='Key_symptoms_TH')
+        print(data_symptoms_TH_1)
+        value_symptoms_TH= data_symptoms_TH_1[data_symptoms_TH_1['Values_symptoms_TH'].isna()]
+        list_of_sym_va_th = value_symptoms_TH['Key_symptoms_TH'].tolist()
+        for i in list_of_sym_va_th:
+          data_symptoms_TH_1.fillna(f"['{i}']",limit=1,inplace=True)
+        # สร้างตาราง
+        all_data = pd.DataFrame()
+        list_100 = list(range(0,100))
+        all_data['index'] = list_100
+        all_data = all_data.merge(data_value_TH.reset_index(), how='outer')
+        all_data = all_data.merge(data_symptoms_TH_1.reset_index(), how='outer')
+        all_data.to_csv('all_data_nameandsym.csv', index=False, encoding='utf-8-sig')
         msg = 'New record created successfully'  
         return jsonify(msg)
 
